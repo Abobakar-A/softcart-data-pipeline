810bab263ffd793b4c35902eda8e15b49e229753
Abobakar-A <Abobakar-A@users.noreply.github.com>
2026-07-17 10:41:26 +0000

    deploy: b9a6418ccb12a307443514787db2bce6bd701121

---
+++ compiled/softcart/models/staging/stg_returns.sql
@@ -0,0 +1,12 @@
+with source as (
+    select * from softcart_db.raw.returns
+)
+select
+    return_id,
+    order_id,
+    return_date,
+    reason,
+    status,
+    refund_amount
+from source
+qualify row_number() over (partition by return_id order by return_date desc) = 1
